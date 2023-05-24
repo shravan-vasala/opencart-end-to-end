8b4009f4b7aafb2e96a7640eaa37305869dcdae5
shravan <shravanvasala999@gmail.com>
2023-05-24 17:33:08 +0530

    Added logs, reports, config.properties

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,32 +1,53 @@
 package testBase;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
+import java.util.ResourceBundle;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	
-	public WebDriver driver;
+	public static WebDriver driver;
 	public Logger logger;
+	public ResourceBundle rb;
 	
+	@Parameters({"browser"})
 	@BeforeClass
-	public void setup() {
+	public void setup(String br) {
 		
 		logger = LogManager.getLogger(this.getClass());
+		rb = ResourceBundle.getBundle("config");
 		
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		if (br.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+
+		} else if (br.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else {
+			driver = new ChromeDriver();
+		}
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("http://localhost/opencart/upload/index.php");
+		driver.get(rb.getString("appURL"));
 		driver.manage().window().maximize();
 	}
 	
@@ -49,6 +70,23 @@ public class BaseClass {
 		String alp = RandomStringUtils.randomAlphabetic(3);
 		String num = RandomStringUtils.randomNumeric(3);
 		return alp + "@" + num;
+	}
+	
+	public String captureScreen(String tname) throws IOException {
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMddhhmmss");
+		Date date = new Date();
+		String timeStamp = format.format(date);
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir") + "\\screenshots\\" + tname +"_" + timeStamp + ".png";
+		File dest = new File(destination);
+		try {
+			FileUtils.copyFile(source, dest);
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return destination;
 	}
 }
 
